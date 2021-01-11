514a38e6bca7275fb806667dbd908d35a2cc7a3e
cpadua912 <cristian.padua912@gmail.com>
2021-01-11 18:45:38 -0500

    Added the frontend

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,15 +1,15 @@
 pragma solidity ^0.5.0;
 
 contract SimpleStorage {
-    string public data;
+    uint256 data;
 
     // Transaction function
-    function set(string memory _data) public {
+    function set(uint256 _data) public {
         data = _data;
     }
 
     // Call function
-    function get() public view returns (string memory) {
+    function get() public view returns (uint256) {
         return data;
     }
 }
--- test/simpleStorage.js
+++ test/simpleStorage.js
@@ -3,8 +3,8 @@ const SimpleStorage = artifacts.require("SimpleStorage");
 contract("SimpleStorage", () => {
   it("Should set the value of data variable in smart contract", async () => {
     const simpleStorage = await SimpleStorage.deployed();
-    await simpleStorage.set("this");
+    await simpleStorage.set(4);
     const result = simpleStorage.get();
-    assert(result, "this");
+    assert(result, 4);
   });
 });
